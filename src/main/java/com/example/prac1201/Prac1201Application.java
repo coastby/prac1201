bc035fee93724d09e0dd3d5f7a016c66d72ca7f5
JO YEJI <whdpwl2@gmail.com>
2022-12-01 14:22:15 +0900

    build: project build

---
+++ src/main/java/com/example/prac1201/Prac1201Application.java
@@ -0,0 +1,13 @@
+package com.example.prac1201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac1201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac1201Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prac1201/Prac1201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac1201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac1201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
